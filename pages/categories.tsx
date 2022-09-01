249936a90750e9adc5c7a3b812e81da34540a1a4
matheustakasaki <matheus8antunes@gmail.com>
2022-09-01 14:48:57 -0300

    Implementando Client Side Fetching

--- pages/categories.tsx
+++ pages/categories.tsx
@@ -1,4 +1,45 @@
 import React from "react";
 import { GetStaticProps } from "next/types";
-import api from '../service/apiInterna.json'
+import apiInterna from '../service/apiInterna.json'
+import { Container } from "../styles/Dashboard/styles";
 
+interface ICategories {
+    id: number;
+    title: string
+}
+
+interface CategoryProps {
+    categories: ICategories[]
+}
+
+export default function categories({ categories }: CategoryProps) {
+    return (
+        <Container>
+            <h1>Categorias</h1>
+            <section>
+                <ul>
+                    {categories.map(category => {
+                        return (
+                            <li key={category.id}>
+                                {category.title}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </section>
+        </Container>
+    )
+};
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async () => {
+    const response = await apiInterna.get(`http://localhost:3333/categories`);
+
+    const categories = await response.data;
+
+    return {
+        props: {
+            categories
+        },
+        revalidate: 5
+    }
+}
